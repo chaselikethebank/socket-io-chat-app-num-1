c8531038c03bc795655edd9249ce24368fc4578c
Chase <chasedemaster.tech@gmail.com>
2024-03-07 21:07:30 -0600

    brought back to working order

--- index.js
+++ index.js
@@ -7,18 +7,24 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
-  console.log('user connected');
-
+  // console.log('user connected')
   socket.on("chat message", (msg) => {
     console.log("message: " + msg);
-    io.emit('chat message', msg);
   });
 });
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+
 
 server.listen(PORT, () => {
   console.log(`listening on *:${PORT}`);
